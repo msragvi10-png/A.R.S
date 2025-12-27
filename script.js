87b0a959adf153d38ab7809e9667ec33ed8dfdb5
msragvi10-png <msragvi10@gmail.com>
2025-12-26 22:51:27 -0600

    Update script.js
    fixing graphic on homepg

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-/* CUSTOM CURSOR */
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
   cursor.style.left = e.clientX + 'px';
   cursor.style.top = e.clientY + 'px';
-});
-
-/* ACTIVE NAV */
-const sections = document.querySelectorAll('section');
-const navLinks = document.querySelectorAll('.side-nav a');
-
-window.addEventListener('scroll', () => {
-  let current = '';
-
-  sections.forEach(section => {
-    const sectionTop = section.offsetTop - 200;
-    if (pageYOffset >= sectionTop) {
-      current = section.getAttribute('id');
-    }
-  });
-
-  navLinks.forEach(link => {
-    link.classList.remove('active');
-    if (link.getAttribute('href') === `#${current}`) {
-      link.classList.add('active');
-    }
-  });
 });
